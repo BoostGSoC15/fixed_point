8c7f05126dca1b0ca6a62b6dc80f0790c87e894a
ckormanyos <e_float@yahoo.com>
2015-07-26 00:02:49 +0200

    Refactor comments. Use include strategy (alphabetic, then libraries).

--- test/test_fixed_point_bit_pattern.cpp
+++ test/test_fixed_point_bit_pattern.cpp
@@ -7,26 +7,21 @@
 //  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-
-
 //! \file
-//!\ This is used to test the underlying bit representation of our negatable class. If these tests fail, something's wrong at
-//!\ the very basic level.
+//!\ This is used to test the underlying bit representation of our negatable class.
+//!\ If any of these tests fail, then something is wrong at a very basic level.
 
 #define BOOST_TEST_MODULE fixed_point_basic
 #define BOOST_LIB_DIAGNOSTIC
-//  Linking to lib file: libboost_unit_test_framework-vc120-mt-gd-1_59.lib
 
-#include <boost/test/included/unit_test.hpp> // No Boost.Test library.
-//#include <boost/test/unit_test.hpp> // USe Boost.Test library.
-
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <sstream>
 #include <string>
-#include <boost/multiprecision/cpp_dec_float.hpp>
 
-#include "boost/fixed_point/fixed_point.hpp"
+#include <boost/fixed_point/fixed_point.hpp>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/test/included/unit_test.hpp> // No Boost.Test library.
 
 typedef boost::multiprecision::number<boost::multiprecision::cpp_dec_float<100>, boost::multiprecision::et_off> cpp_dec_float_100_noet;
 typedef boost::multiprecision::number<boost::multiprecision::cpp_dec_float<200>, boost::multiprecision::et_off> cpp_dec_float_200_noet;
@@ -35,11 +30,11 @@ BOOST_AUTO_TEST_CASE(fixed_point_construct_nearest_even_round)
 {
   //! small range and resolution
   {
-  	typedef boost::fixed_point::negatable<4, -2, boost::fixed_point::round::nearest_even> fixed_point_type_nearest_even_round;
+    typedef boost::fixed_point::negatable<4, -2, boost::fixed_point::round::nearest_even> fixed_point_type_nearest_even_round;
     //! max precision + 1/2 ULP
-    fixed_point_type_nearest_even_round x = fixed_point_type_nearest_even_round (-15.375);
-    fixed_point_type_nearest_even_round y = fixed_point_type_nearest_even_round(-15.5);    
-    
+    fixed_point_type_nearest_even_round x = fixed_point_type_nearest_even_round(-15.375);
+    fixed_point_type_nearest_even_round y = fixed_point_type_nearest_even_round(-15.5);
+
     std::string bit_pattern = "1000010";
 
     BOOST_CHECK_EQUAL(x.bit_pattern (), bit_pattern);
@@ -57,7 +52,7 @@ BOOST_AUTO_TEST_CASE(fixed_point_construct_nearest_even_round)
 
   //! larger range and small resolution
   {
-  	typedef boost::fixed_point::negatable<87, -4, boost::fixed_point::round::nearest_even> fixed_point_type_nearest_even_round;
+    typedef boost::fixed_point::negatable<87, -4, boost::fixed_point::round::nearest_even> fixed_point_type_nearest_even_round;
     fixed_point_type_nearest_even_round x = fixed_point_type_nearest_even_round (cpp_dec_float_100_noet("154742504910672534362390527.375"));
     //! max precision + 1/2 ULP
     fixed_point_type_nearest_even_round y = fixed_point_type_nearest_even_round (cpp_dec_float_100_noet("154742504910672534362390527.34375"));
@@ -75,7 +70,7 @@ BOOST_AUTO_TEST_CASE(fixed_point_construct_nearest_even_round)
 
     BOOST_CHECK_EQUAL(x.bit_pattern (), bit_pattern);
     BOOST_CHECK_EQUAL(y.bit_pattern (), bit_pattern);
-  }  
+  }
 
   //! small range and larger resolution
   {
@@ -120,7 +115,5 @@ BOOST_AUTO_TEST_CASE(fixed_point_construct_nearest_even_round)
 
     BOOST_CHECK_EQUAL(x.bit_pattern (), bit_pattern);
     BOOST_CHECK_EQUAL(y.bit_pattern (), bit_pattern);
-  }    
-
-
+  }
 }
