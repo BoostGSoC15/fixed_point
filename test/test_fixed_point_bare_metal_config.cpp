9a5ce797f7174b63136c47f538de5ca8a2868abf
ckormanyos <e_float@yahoo.com>
2015-07-30 21:34:25 +0200

    Add tests for sqrt and bare metal configuration.

---
+++ test/test_fixed_point_bare_metal_config.cpp
@@ -0,0 +1,89 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2015.
+//  Copyright Nikhar Agrawal 2015.
+//  Copyright Paul Bristow 2015.
+//  Distributed under the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+//! \file
+//!\brief Tests for a successful bare-metal configuration of fixed_point.
+
+// For further mathematical details pertaining to this file,
+// see C.M. Kormanyos, Real-Time C++: Efficient Object-Oriented and
+// Template Microcontroller Programming (Springer, Heidelberg, 2013).
+// in Section 12.7 and Chapter 13.
+// See http://link.springer.com/chapter/10.1007/978-3-642-34688-0_13
+
+#define BOOST_TEST_MODULE fixed_point_bare_metal_config
+#define BOOST_LIB_DIAGNOSTIC
+
+// Configure Boost.Fixed_point for a bare-metal system.
+
+#define BOOST_FIXED_POINT_DISABLE_MULTIPRECISION // Do not use Boost.Multiprecision.
+#define BOOST_FIXED_POINT_DISABLE_IOSTREAM       // Do not use I/O streaming.
+
+#include <boost/fixed_point/fixed_point.hpp>
+#include <boost/test/included/unit_test.hpp>
+
+namespace local
+{
+  template<typename RealValueType,
+           typename RealFunctionType>
+  RealValueType first_derivative(const RealValueType& x,
+                                 const RealValueType& dx,
+                                 RealFunctionType real_function)
+  {
+    // Evaluate first derivative of real_function using
+    // a three-point central-difference rule of O(dx^6).
+
+    const RealValueType dx2(dx  + dx);
+    const RealValueType dx3(dx2 + dx);
+
+    const RealValueType m1((  real_function(x + dx)
+                            - real_function(x - dx))  / 2U);
+    const RealValueType m2((  real_function(x + dx2)
+                            - real_function(x - dx2)) / 4U);
+    const RealValueType m3((  real_function(x + dx3)
+                            - real_function(x - dx3)) / 6U);
+
+    const RealValueType fifteen_m1(m1 * 15U);
+    const RealValueType six_m2    (m2 *  6U);
+    const RealValueType ten_dx    (dx * 10U);
+
+    return ((fifteen_m1 - six_m2) + m3) / ten_dx;
+  }
+} // namespace local
+
+BOOST_AUTO_TEST_CASE(fixed_point_bare_metal_config)
+{
+  typedef boost::fixed_point::negatable<6, -9> fixed_point_type;
+
+  const fixed_point_type a = fixed_point_type(12) / 10;
+  const fixed_point_type b = fixed_point_type(34) / 10;
+  const fixed_point_type c = fixed_point_type(56) / 10;
+
+  // Compute the approximate derivative of [(a * x^2) + (b * x) + c]
+  // evaluated at 1/2, where the approximate values of the coefficients
+  // are: a = 1.2, b = 3.4, and c = 5.6. The numerical tolerance is set
+  // to a value of approximately 1/4.
+
+  const fixed_point_type d =
+    local::first_derivative(fixed_point_type(1) / 2,  // x
+                            fixed_point_type(1) / 4,  // Step size dx.
+                            [&a, &b, &c](const fixed_point_type& x) -> fixed_point_type
+                            {
+                              return (((a * x) + b) * x) + c;
+                            });
+
+  // The expected result is ((2 * a) + b) = (1.2 + 3.4) = 4.6 (exact).
+  // We obtain a fixed-point result of approximately 4.5938.
+
+  // Verify that the result lies within (4.5 < result < 4.7).
+  // The expected result is 4.6, so this is a wide tolerance.
+
+  const bool result_is_ok = ((d > (fixed_point_type(45) / 10)) && (d < (fixed_point_type(47) / 10)));
+
+  BOOST_CHECK_EQUAL(result_is_ok, true);
+}
---
+++ test/test_fixed_point_sqrt.cpp
@@ -0,0 +1,96 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright Christopher Kormanyos 2015.
+//  Copyright Nikhar Agrawal 2015.
+//  Copyright Paul Bristow 2015.
+//  Distributed under the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+//! \file
+//!\brief Tests for sqrt(fixed_point). Along the way, also test numerous fixed_point arithmetic operations.
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+#define BOOST_TEST_MODULE fixed_point_sqrt
+#define BOOST_LIB_DIAGNOSTIC
+
+#include <boost/fixed_point/fixed_point.hpp>
+#include <boost/math/constants/constants.hpp>
+#include <boost/test/included/unit_test.hpp>
+
+namespace local
+{
+  template<typename FixedPointType,
+           typename FloatPointType = FixedPointType::float_type>
+  void test(const FixedPointType& my_tolerance = ldexp(FixedPointType(1), FixedPointType::resolution + 3))
+  {
+    // Use at least 6 resolution bits.
+    // Use at least 8 range bits.
+
+    BOOST_STATIC_ASSERT(-FixedPointType::resolution >= 6);
+    BOOST_STATIC_ASSERT( FixedPointType::range      >= 8);
+
+    using std::sqrt;
+
+    /*
+    std::cout << "Testing sqrt for negatable<"
+              << std::setw(3)
+              << FixedPointType::range
+              << ", "
+              << std::setw(4)
+              << FixedPointType::resolution
+              << ">."
+              << std::endl;
+    */
+
+    const FixedPointType a1(  2    );                                       const FloatPointType b1(  2    );
+    const FixedPointType a2(  3    );                                       const FloatPointType b2(  3    );
+    const FixedPointType a3(  8.375);                                       const FloatPointType b3(  8.375);
+    const FixedPointType a4( 64.125);                                       const FloatPointType b4( 64.125);
+    const FixedPointType a5(100.875);                                       const FloatPointType b5(100.875);
+    const FixedPointType a6(FixedPointType(  1) /  3);                      const FloatPointType b6(FloatPointType(  1) /  3);
+    const FixedPointType a7(FixedPointType( 12) / 10);                      const FloatPointType b7(FloatPointType( 12) / 10);
+    const FixedPointType a8(FixedPointType(111) / 10);                      const FloatPointType b8(FloatPointType(111) / 10);
+    const FixedPointType a9(boost::math::constants::phi<FixedPointType>()); const FloatPointType b9(boost::math::constants::phi<FloatPointType>());
+
+    BOOST_CHECK_CLOSE_FRACTION(sqrt(a1), FixedPointType(sqrt(b1)), my_tolerance);
+    BOOST_CHECK_CLOSE_FRACTION(sqrt(a2), FixedPointType(sqrt(b2)), my_tolerance);
+    BOOST_CHECK_CLOSE_FRACTION(sqrt(a3), FixedPointType(sqrt(b3)), my_tolerance);
+    BOOST_CHECK_CLOSE_FRACTION(sqrt(a4), FixedPointType(sqrt(b4)), my_tolerance);
+    BOOST_CHECK_CLOSE_FRACTION(sqrt(a5), FixedPointType(sqrt(b5)), my_tolerance);
+    BOOST_CHECK_CLOSE_FRACTION(sqrt(a6), FixedPointType(sqrt(b6)), my_tolerance);
+    BOOST_CHECK_CLOSE_FRACTION(sqrt(a7), FixedPointType(sqrt(b7)), my_tolerance);
+    BOOST_CHECK_CLOSE_FRACTION(sqrt(a8), FixedPointType(sqrt(b8)), my_tolerance);
+    BOOST_CHECK_CLOSE_FRACTION(sqrt(a9), FixedPointType(sqrt(b9)), my_tolerance);
+  }
+}
+
+BOOST_AUTO_TEST_CASE(fixed_point_sqrt)
+{
+  using boost::fixed_point::negatable;
+  using boost::fixed_point::round::fastest;
+  using boost::fixed_point::round::nearest_even;
+
+  { typedef negatable<  8,   -7, fastest> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable<  8,  -15, fastest> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable<  8,  -44, fastest> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable<  8,  -55, fastest> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable< 20,  -10, fastest> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable<100,  -10, fastest> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable< 20, -100, fastest> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable<100, -100, fastest> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable< 20, -491, fastest> fixed_point_type; local::test<fixed_point_type>(ldexp(fixed_point_type(1), fixed_point_type::resolution + 16)); }
+
+  { typedef negatable<  8,   -7, nearest_even> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable<  8,  -15, nearest_even> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable<  8,  -44, nearest_even> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable<  8,  -55, nearest_even> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable< 20,  -10, nearest_even> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable<100,  -10, nearest_even> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable< 20, -100, nearest_even> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable<100, -100, nearest_even> fixed_point_type; local::test<fixed_point_type>(); }
+  { typedef negatable< 20, -491, nearest_even> fixed_point_type; local::test<fixed_point_type>(ldexp(fixed_point_type(1), fixed_point_type::resolution + 16)); }
+}
