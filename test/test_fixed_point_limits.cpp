824cdb315058544ca8854c3e11efe44fb3c850e9
pabristow <pbristow@hetp.u-net.com>
2015-07-30 10:34:44 +0100

    Merge OK with epsilon correction and added some tests for epsilon.

--- test/test_fixed_point_limits.cpp
+++ test/test_fixed_point_limits.cpp
@@ -22,6 +22,8 @@
 
 #include "boost/fixed_point/fixed_point.hpp"
 
+using namespace boost::fixed_point;
+
 BOOST_AUTO_TEST_CASE(fixed_point_numeric_limits_min)
 { 
   {//! small range 2 and resolution 5 + sign = 8 bit, default round and overflow.
@@ -116,18 +118,69 @@ BOOST_AUTO_TEST_CASE(fixed_point_numeric_limits_misc)
 
 
 BOOST_AUTO_TEST_CASE(fixed_point_numeric_limits_value_float)
-{
-  {
+{ // Test the types for value_type and float_type for some fixed_point types.
+  { // negatable<2, -5> using eight bits.
     typedef boost::fixed_point::negatable<2, -5> fixed_point_type_2m5;
 
-    std::cout << typeid(fixed_point_type_2m5::value_type).name() << std::endl;
+    //std::cout << typeid(fixed_point_type_2m5::value_type).name() << std::endl;
     BOOST_CHECK_EQUAL(typeid(fixed_point_type_2m5::value_type).name(), "signed char");
 
-    std::cout << typeid(fixed_point_type_2m5::float_type).name() << std::endl;
+    //std::cout << typeid(fixed_point_type_2m5::float_type).name() << std::endl;
     BOOST_CHECK_EQUAL(typeid(fixed_point_type_2m5::float_type).name(), "float");
   }
 
+  { // negatable<0, -31>
+    typedef boost::fixed_point::negatable<0, -31> fixed_point_type_0m31;
+
+    //std::cout << typeid(fixed_point_type_0m31::value_type).name() << std::endl;
+    BOOST_CHECK_EQUAL(typeid(fixed_point_type_0m31::value_type).name(), "int");
+
+   // std::cout << typeid(fixed_point_type_0m31::float_type).name() << std::endl;
+    BOOST_CHECK_EQUAL(typeid(fixed_point_type_0m31::float_type).name(), "double");
+  }
+// More example here needed.
 
 } // BOOST_AUTO_TEST_CASE(fixed_point_numeric_limits_value_float)
 
 
+BOOST_AUTO_TEST_CASE(fixed_point_numeric_limits_epsilon)
+{ // Test epsilon is 2^-(significand bits - 1)
+  {
+    typedef boost::fixed_point::negatable<0, -31> fixed_point_type_0m31;
+
+   // std::cout << std::numeric_limits<fixed_point_type_0m31>::epsilon() << std::endl;  // 2^-(31-1)
+    BOOST_CHECK_EQUAL(std::numeric_limits<fixed_point_type_0m31>::epsilon(), 9.3132257462e-010);
+  }
+
+  {
+    typedef boost::fixed_point::negatable<0, -7> fixed_point_type_0m7;
+    BOOST_CHECK_EQUAL(std::numeric_limits<fixed_point_type_0m7>::epsilon(), 0.015625); // 2^-(7-1) = 2^-6
+  }
+  {
+    typedef boost::fixed_point::negatable<1, -6> fixed_point_type_1m6;
+    BOOST_CHECK_EQUAL(std::numeric_limits<fixed_point_type_1m6>::epsilon(), 0.03125); // 2^-(6-1) = 2^-5
+  }
+  { // Note that if do not use a typedef then
+    // extra brackets needed to avoid confusing BOOST_CHECK_EQUAL claiming wrong number of arguments.
+    BOOST_CHECK_EQUAL((std::numeric_limits<negatable<1, -6> >::epsilon()), 0.03125); // 2^-(6-1) = 2^-5
+  }
+  { // Edge cases
+    std::cout << std::numeric_limits<negatable<0, -1>>::epsilon() << std::endl;  // 1
+
+    // BOOST_CHECK_EQUAL((std::numeric_limits<negatable<0, -1> >::epsilon()), 1); // 2^-(1-1) = 2^0 = 1 
+    // error : in "fixed_point_numeric_limits_epsilon": check (std::numeric_limits<negatable<0, -1> >::epsilon()) == 1 has failed [1 != 1]
+    // negatable<0, -1> x(1);
+    // std::cout << x << std::endl; // 0 so 1 cannot be represented in this type?
+    // BOOST_CHECK_EQUAL((std::numeric_limits<negatable<0, -1> >::epsilon()), x); // 2^-(1-1) = 2^0 = 1 
+    // error : in "fixed_point_numeric_limits_epsilon": check (std::numeric_limits<negatable<0, -1> >::epsilon()) == x has failed [1 != 0]
+
+    BOOST_CHECK_EQUAL((std::numeric_limits<negatable<1, -1> >::epsilon()), 1); // 2^-(1-1) = 2^0 = 1
+    BOOST_CHECK_EQUAL((std::numeric_limits<negatable<2, -1> >::epsilon()), 1); // 
+    BOOST_CHECK_EQUAL((std::numeric_limits<negatable<3, -1> >::epsilon()), 1); // 
+    BOOST_CHECK_EQUAL((std::numeric_limits<negatable<4, -1> >::epsilon()), 1); //
+    BOOST_CHECK_EQUAL((std::numeric_limits<negatable<5, -1> >::epsilon()), 1); //
+    BOOST_CHECK_EQUAL((std::numeric_limits<negatable<6, -1> >::epsilon()), 1); //
+  }
+
+
+}
