616d9d172bf54348a5d5449da43a9cdb9ac5d8f0
ckormanyos <e_float@yahoo.com>
2015-08-16 01:56:22 +0200

    Repair this test case for two decimal digits of precision.

--- test/test_negatable_round_trip_digits02_005m2.cpp
+++ test/test_negatable_round_trip_digits02_005m2.cpp
@@ -18,31 +18,41 @@
 #include <sstream>
 #include <string>
 
-#include <boost/cstdint.hpp>
 #include <boost/fixed_point/fixed_point.hpp>
-#include <boost/lexical_cast.hpp>
 #include <boost/test/included/unit_test.hpp>
 
 namespace local
 {
   // Define a binary fixed-point type with resolution 5 range -2
-  typedef
-  boost::fixed_point::negatable<5,
-                                -2,
-                                boost::fixed_point::round::nearest_even>
-  fixed_point_type_decimal_digits_005m2;
+  typedef boost::fixed_point::negatable<5, -2> fixed_point_type;
 
-  bool round_trip(const fixed_point_type_decimal_digits_005m2& x);
+  bool round_trip(const fixed_point_type& x);
+
+  // Here we need to invert Kahan's formula to ensure that all of
+  // the fractional digits can be printed in decimal form.
+  // In this case, we need to print two decimal digits10,
+  // even tough we are only have two binary digits2.
+
+  // Begin with
+  //   digits2 = 8 (what we would have needed for two decimal digits)
+  // This gives
+  //  digits10 = [(8 - 1) * 301] / 1000
+  //  digits10 = 2107 / 1000 = 2
+
+  // In this case, we simply hard-code the value of 2 for decimal digits
+  // in the fixed-point field.
+  BOOST_CONSTEXPR_OR_CONST int digits10 = (-fixed_point_type::resolution);
 }
 
-bool local::round_trip(const local::fixed_point_type_decimal_digits_005m2& x)
+bool local::round_trip(const local::fixed_point_type& x)
 {
-  typedef local::fixed_point_type_decimal_digits_005m2 fixed_point_type;
+  typedef local::fixed_point_type fixed_point_type;
 
   std::stringstream ss1;
 
-  ss1 << std::setprecision(std::numeric_limits<fixed_point_type>::digits10)
-      << std::fixed
+  ss1.precision(local::digits10);
+
+  ss1 << std::fixed
       << x;
 
   std::stringstream ss2(ss1.str());
@@ -57,7 +67,7 @@ bool local::round_trip(const local::fixed_point_type_decimal_digits_005m2& x)
 
 BOOST_AUTO_TEST_CASE(test_negatable_round_trip_digits02_005m2)
 {
-  typedef local::fixed_point_type_decimal_digits_005m2 fixed_point_type;
+  typedef local::fixed_point_type      fixed_point_type;
   typedef fixed_point_type::float_type floating_point_type;
 
   boost::uint_fast16_t count;
@@ -73,14 +83,14 @@ BOOST_AUTO_TEST_CASE(test_negatable_round_trip_digits02_005m2)
     std::stringstream ss1;
 
     ss1 << std::fixed
-        << std::setprecision(2)
+        << std::setprecision(local::digits10)
         << (float(count) / 4.0F);
 
     std::string str(ss1.str());
 
     const fixed_point_type x(boost::lexical_cast<floating_point_type>(str));
 
-    const bool next_test_result = local::round_trip(local::fixed_point_type_decimal_digits_005m2(x));
+    const bool next_test_result = local::round_trip(local::fixed_point_type(x));
 
     b = (b && next_test_result);
   }
