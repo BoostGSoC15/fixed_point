384d5535422b2c6a034a7f8fa6bbf6f874b75e9a
ckormanyos <e_float@yahoo.com>
2015-08-15 22:59:22 +0200

    Improve the Bernoulli test case.

--- test/test_negatable_bernoullib2n.cpp
+++ test/test_negatable_bernoullib2n.cpp
@@ -9,12 +9,10 @@
 //! \file
 //!\brief Tests for bernoulli_b2n(fixed_point). Along the way, also test numerous fixed_point arithmetic operations.
 
-#define BOOST_TEST_MODULE test_negatable_bernoullib2n
+#define BOOST_TEST_MODULE test_negatable_bernoulli_b2n
 #define BOOST_LIB_DIAGNOSTIC
 
 #include <algorithm>
-#include <cmath>
-#include <iterator>
 #include <vector>
 #include <utility>
 
@@ -22,16 +20,20 @@
 #include <boost/math/special_functions/bernoulli.hpp>
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(test_negatable_bernoullib2n)
+BOOST_AUTO_TEST_CASE(test_negatable_bernoulli_b2n)
 {
-  // Test bernoulli_b2n() for negatable in one single digit range.
-  // Compute 23 fixed-point Bernoulli numbers.
-  // Start with Bernoulli number 0.
+  // Test bernoulli_b2n() for negatable. Compute an array of
+  // fixed-point Bernoulli numbers and test them with
+  // multiprecision floating-point control values.
 
-  BOOST_CONSTEXPR_OR_CONST std::size_t number_of_bernoulli_b2n = 23U;
+  BOOST_CONSTEXPR_OR_CONST std::size_t number_of_bernoulli_b2n = 22U;
 
-  typedef boost::fixed_point::negatable<1847, -200> fixed_point_type;
-  typedef fixed_point_type::float_type              float_point_type;
+  typedef boost::fixed_point::negatable<2047 - 256,
+                                        -256,
+                                        boost::fixed_point::round::nearest_even>
+  fixed_point_type;
+
+  typedef fixed_point_type::float_type float_point_type;
 
   // Define fixed-point and floating-point verctors for calculating Bernoulli numbers.
   std::vector<fixed_point_type> b2n_fixed;
@@ -41,15 +43,20 @@ BOOST_AUTO_TEST_CASE(test_negatable_bernoullib2n)
   boost::math::bernoulli_b2n<fixed_point_type>(0, number_of_bernoulli_b2n, std::back_inserter(b2n_fixed));
   boost::math::bernoulli_b2n<float_point_type>(0, number_of_bernoulli_b2n, std::back_inserter(b2n_float));
 
-  // Create smaller fixed-point and floating-point types for the results.
-  typedef boost::fixed_point::negatable<255, -256> fixed_point_result_type;
-  typedef fixed_point_type::float_type             float_point_result_type;
+  // Create less-wide fixed-point and floating-point types for manipulating the results.
+  typedef boost::fixed_point::negatable< 255,
+                                        -256,
+                                        boost::fixed_point::round::nearest_even>
+  fixed_point_result_type;
 
-  std::vector<fixed_point_result_type> b2n_fixed_result(b2n_fixed.size());
-  std::vector<float_point_result_type> b2n_float_result(b2n_float.size());
+  typedef fixed_point_type::float_type float_point_result_type;
 
-  // Transform the Bernoulli number arrays. Thereby testing the fixed-point class
-  // with yet another algorithm.
+  std::vector<fixed_point_result_type> b2n_fixed_result(number_of_bernoulli_b2n);
+  std::vector<float_point_result_type> b2n_float_result(number_of_bernoulli_b2n);
+
+  // Transform the Bernoulli number arrays. This tests the fixed-point
+  // class populating an STL container in combination with an algorithm
+  // and a lambda function.
   std::transform(b2n_fixed.cbegin(),
                  b2n_fixed.cend(),
                  b2n_fixed_result.begin(),
@@ -67,10 +74,11 @@ BOOST_AUTO_TEST_CASE(test_negatable_bernoullib2n)
                  });
 
   // Set a judiciously selected tolerance for these tests.
-  const fixed_point_result_type tolerance = ldexp(fixed_point_result_type(1), -130);
+  const fixed_point_result_type tolerance = ldexp(fixed_point_result_type(1), -160);
 
-  // Search for a potential mismatch between the fixed-point Bernoulli numbers
-  // and the floatiing-point Bernoulli numbers
+  // Search for a potential mismatch between the fixed-point
+  // Bernoulli numbers and the floating-point Bernoulli numbers
+
   const std::pair<std::vector<fixed_point_result_type>::const_iterator,
                   std::vector<float_point_result_type>::const_iterator> result_of_mismatch_search =
     std::mismatch(b2n_fixed_result.cbegin(),
@@ -83,7 +91,7 @@ BOOST_AUTO_TEST_CASE(test_negatable_bernoullib2n)
 
                     const bool comparison_is_ok = (delta <= tolerance);
 
-                    BOOST_CHECK(comparison_is_ok);
+                    BOOST_CHECK_EQUAL(comparison_is_ok, true);
 
                     return comparison_is_ok;
                   });
