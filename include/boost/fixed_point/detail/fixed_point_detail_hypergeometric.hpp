4b9b7bd69b4ce573045402ecfbf09aad593e4288
ckormanyos <e_float@yahoo.com>
2015-09-28 22:02:52 +0200

    Repair comments. Add series calculation of 2^x.

--- include/boost/fixed_point/detail/fixed_point_detail_hypergeometric.hpp
+++ include/boost/fixed_point/detail/fixed_point_detail_hypergeometric.hpp
@@ -7,9 +7,10 @@
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-// fixed_point_detail_constants.hpp implements templates
-// for computing fixed-point representations of mathematical
-// constants such as pi and log(2).
+// fixed_point_detail_hypergeometric.hpp implements templates
+// for computing hypergeometric series used for Taylor-series-like
+// expansions. These are used by certain <cmath> functions to
+// simplify typing and reduce the complexity of the code.
 
 #ifndef FIXED_POINT_DETAIL_HYPERGEOMETRIC_2015_08_21_HPP_
   #define FIXED_POINT_DETAIL_HYPERGEOMETRIC_2015_08_21_HPP_
@@ -152,6 +153,49 @@
     }
 
     return h2f1;
+  }
+
+  template<typename NumericType>
+  NumericType two_to_the_power_of_x(const NumericType& x,
+                                    const NumericType& my_ln_two)
+  {
+    // Compute the series representation of (2^x),
+    // which is very closely related to a hypergeometric
+    // series.
+
+    // There are no checks on input range or parameter boundaries
+    // in this series calculation.
+
+    // As such, this function is designed for small-argument
+    // Taylor-series-like expansions only. It is not intended
+    // for general purpose calculations of (2^x).
+
+    NumericType ln_two_times_x(my_ln_two * x);
+    NumericType term          (ln_two_times_x);
+    NumericType sum           (1 + term);
+
+    BOOST_CONSTEXPR_OR_CONST boost::uint32_t maximum_number_of_iterations = UINT32_C(10000);
+
+    // Perform the series expansion of (2^x).
+    for(boost::uint32_t n = UINT32_C(2); n < maximum_number_of_iterations; ++n)
+    {
+      term *= ln_two_times_x;
+      term /= n;
+
+      const bool minimum_number_of_iterations_is_complete = (n > UINT32_C(4));
+
+      using std::fabs;
+
+      if(   (minimum_number_of_iterations_is_complete)
+         && (fabs(term) <= std::numeric_limits<NumericType>::epsilon()))
+      {
+        break;
+      }
+
+      sum += term;
+    }
+
+    return sum;
   }
 
   } } } // namespace boost::fixed_point::detail
