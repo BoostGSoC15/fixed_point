26abef8d4a0ed0dbc523d16cd61b9b04c42c764b
ckormanyos <e_float@yahoo.com>
2015-11-19 20:42:03 +0100

    Switch negatable<0, 7> in to <1, 6> in test.

--- test/test_negatable_func_next_prior.cpp
+++ test/test_negatable_func_next_prior.cpp
@@ -53,8 +53,8 @@ void tests(T value)
 
 BOOST_AUTO_TEST_CASE(test_negatable_func_next_prior)
 {
-  typedef boost::fixed_point::negatable< 2,   -5> fixed_point_type_2m5;        //  8-bit fixed_point type 2 range 5 resolution.
-  typedef boost::fixed_point::negatable< 0,   -7> fixed_point_type_0m7;        //  8-bit fixed_point type - all resolution bits used.
+  typedef boost::fixed_point::negatable< 2,   -5> fixed_point_type_2m5;    //  8-bit fixed_point type 2 range 5 resolution.
+  typedef boost::fixed_point::negatable< 1,   -6> fixed_point_type_1m6;    //  8-bit fixed_point type - all resolution bits except one are used.
   typedef boost::fixed_point::negatable< 7,   -8> fixed_point_type;        //  16-bit fixed_point type.
   typedef boost::fixed_point::negatable<15,  -48> fixed_point_type_15m48;  //  64-bit fixed_point type.
   typedef boost::fixed_point::negatable<15, -240> fixed_point_type_15m240; // 256-bit using multiprecision
@@ -128,7 +128,7 @@ BOOST_AUTO_TEST_CASE(test_negatable_func_next_prior)
 
   tests(fixed_point_type_2m5(0));
 
-  tests(fixed_point_type_0m7(0));  // fails!
+  tests(fixed_point_type_1m6(0));
 
 
 } // BOOST_AUTO_TEST_CASE(test_negatable_func_next_prior)
